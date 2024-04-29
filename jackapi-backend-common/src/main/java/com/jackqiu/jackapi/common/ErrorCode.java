75a01db41fb88218c6b128a4872d499cc8e2f10a
jackqiu <2795275206@qq.com>
2024-04-29 21:50:59 +0800

    设置dev分支

--- jackapi-backend-common/src/main/java/com/jackqiu/jackapi/common/ErrorCode.java
+++ jackapi-backend-common/src/main/java/com/jackqiu/jackapi/common/ErrorCode.java
@@ -7,7 +7,7 @@ package com.jackqiu.jackapi.common;
  */
 public enum ErrorCode {
 
-    SUCCESS(0, "ok"),
+    SUCCESS(0, "ok"),//dev
     PARAMS_ERROR(40000, "请求参数错误"),
     NOT_LOGIN_ERROR(40100, "未登录"),
     NO_AUTH_ERROR(40101, "无权限"),
